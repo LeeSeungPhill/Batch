f87fb25588c120b3071df8bafc3e3e928ead6c93
LeeSeungPhill <125461810+LeeSeungPhill@users.noreply.github.com>
2025-04-01 22:19:30 +0900

    보유종목 전체조회 보완

--- terrabot.py
+++ terrabot.py
@@ -1480,8 +1480,7 @@ def callback_get(update, context) :
                     sell_command = f"/hsell_{i[6]}_{avail_amount}"
                     company = i[7] + "[" + i[6] + "]"
             
-                    context.bot.send_message(chat_id=update.effective_chat.id,
-                                                text=company + " : 매입가-" + format(int(purchase_price), ',d') + "원, 매입수량-" + format(purchase_amount, ',d') + "주, 매입금액-" + format(purchase_sum, ',d') + "원, 현재가-" + format(current_price, ',d') + "원, 평가금액-" + format(eval_sum, ',d') + "원, 수익률(" + str(earning_rate) + ")%, 손수익금액(" + format(valuation_sum, ',d') + ")원, 저항가-" + format(sign_resist_price, ',d') + "원, 지지가-" + format(sign_support_price, ',d') + "원, 최종목표가-" + format(end_target_price, ',d') + "원, 최종이탈가-" + format(end_loss_price, ',d') + "원, 매도예정금액-" + format(sell_plan_sum, ',d') + "원(" + format(sell_plan_amount, ',d') + "주), 매도가능수량=" + format(avail_amount, ',d') + "주 => {sell_command}")
+                    context.bot.send_message(chat_id=update.effective_chat.id, text=(f"{company} : 매입가-{format(int(purchase_price), ',d')}원, 매입수량-{format(purchase_amount, ',d')}주, 매입금액-{format(purchase_sum, ',d')}원, 현재가-{format(current_price, ',d')}원, 평가금액-{format(eval_sum, ',d')}원, 수익률({str(earning_rate)})%, 손수익금액({format(valuation_sum, ',d')})원, 저항가-{format(sign_resist_price, ',d')}원, 지지가-{format(sign_support_price, ',d')}원, 최종목표가-{format(end_target_price, ',d')}원, 최종이탈가-{format(end_loss_price, ',d')}원, 매도예정금액-{format(sell_plan_sum, ',d')}원({format(sell_plan_amount, ',d')}주), 매도가능수량-{format(avail_amount, ',d')}주 => {sell_command}"))
             
                     get_handler = CommandHandler(sell_command, get_command3)
                     updater.dispatcher.add_handler(get_handler)
