2464980f75595c42423a270b102a459b5b1812d1
LeeSeungPhill <phills2@hanmail.net>
2025-07-14 22:55:53 +0900

    프로시저 호출 파라메터 수정

--- call_upd_dly_stock_item.py
+++ call_upd_dly_stock_item.py
@@ -7,7 +7,7 @@ conn_string = "dbname='fund_risk_mng' host='localhost' port='5432' user='postgre
 conn = db.connect(conn_string)
 
 cur1 = conn.cursor()
-cur1.execute("CALL upd_dly_stock_item(%s, %s, %s);", ['', ''])
+cur1.execute("CALL upd_dly_stock_item(%s, %s);", ['', ''])
 result = cur1.fetchall()
 cur1.close()
 conn.commit()
