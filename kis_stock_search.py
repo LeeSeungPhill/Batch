11db7c0adf9bbb1e4dce96e554773212c45e97af
LeeSeungPhill <phills2@hanmail.net>
2025-10-20 12:35:18 +0900

    1

--- kis_stock_search.py
+++ kis_stock_search.py
@@ -35,15 +35,6 @@ elif arguments[1] == '3':
 elif arguments[1] == '4':
     search_name = "파워종목"        
 
-cur001 = conn.cursor()
-cur001.execute("select bot_token1 from \"stockAccount_stock_account\" where nick_name = '" + arguments[2] + "'")
-result_001 = cur001.fetchone()
-cur001.close()
-token = result_001[0]
-
-# 텔레그램 연동 토큰값 설정
-bot = telegram.Bot(token)
-
 def auth(APP_KEY, APP_SECRET):
 
     # 인증처리
@@ -57,6 +48,35 @@ def auth(APP_KEY, APP_SECRET):
     ACCESS_TOKEN = res.json()["access_token"]
 
     return ACCESS_TOKEN
+
+cur01 = conn.cursor()
+cur01.execute("select acct_no, access_token, app_key, app_secret, token_publ_date, bot_token1 from \"stockAccount_stock_account\" where nick_name = '" + arguments[2] + "'")
+result_one = cur01.fetchone()
+cur01.close()
+
+acct_no = result_one[0]
+access_token = result_one[1]
+app_key = result_one[2]
+app_secret = result_one[3]
+bot_token = result_one[6]
+
+YmdHMS = datetime.now()
+validTokenDate = datetime.strptime(result_one[4], '%Y%m%d%H%M%S')
+diff = YmdHMS - validTokenDate
+# print("diff : " + str(diff.days))
+if diff.days >= 1:  # 토큰 유효기간(1일) 만료 재발급
+    access_token = auth(app_key, app_secret)
+    token_publ_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    print("new access_token : " + access_token)
+    # 계정정보 토큰값 변경
+    cur02 = conn.cursor()
+    update_query = "update \"stockAccount_stock_account\" set access_token = %s, token_publ_date = %s, last_chg_date = %s where acct_no = %s"
+    # update 인자값 설정
+    record_to_update = ([access_token, token_publ_date, datetime.now(), acct_no])
+    # DB 연결된 커서의 쿼리 수행
+    cur02.execute(update_query, record_to_update)
+    conn.commit()
+    cur02.close()
 
 def inquire_search_result(access_token, app_key, app_secret, id, seq):
 
@@ -79,36 +99,8 @@ def inquire_search_result(access_token, app_key, app_secret, id, seq):
 
 def main(telegram_text):
     chat_id = "2147256258"
-    bot = telegram.Bot(token=token)
+    bot = telegram.Bot(token=bot_token)
     bot.send_message(chat_id, telegram_text)
-
-cur01 = conn.cursor()
-cur01.execute("select acct_no, access_token, app_key, app_secret, token_publ_date from \"stockAccount_stock_account\" where nick_name = '" + arguments[2] + "'")
-result_one = cur01.fetchone()
-cur01.close()
-
-acct_no = result_one[0]
-access_token = result_one[1]
-app_key = result_one[2]
-app_secret = result_one[3]
-
-YmdHMS = datetime.now()
-validTokenDate = datetime.strptime(result_one[4], '%Y%m%d%H%M%S')
-diff = YmdHMS - validTokenDate
-# print("diff : " + str(diff.days))
-if diff.days >= 1:  # 토큰 유효기간(1일) 만료 재발급
-    access_token = auth(app_key, app_secret)
-    token_publ_date = datetime.now().strftime("%Y%m%d%H%M%S")
-    print("new access_token : " + access_token)
-    # 계정정보 토큰값 변경
-    cur02 = conn.cursor()
-    update_query = "update \"stockAccount_stock_account\" set access_token = %s, token_publ_date = %s, last_chg_date = %s where acct_no = %s"
-    # update 인자값 설정
-    record_to_update = ([access_token, token_publ_date, datetime.now(), acct_no])
-    # DB 연결된 커서의 쿼리 수행
-    cur02.execute(update_query, record_to_update)
-    conn.commit()
-    cur02.close()
 
 item_search = inquire_search_result(access_token, app_key, app_secret, 'phills2', arguments[1])  # 종목조건검색 조회
 
