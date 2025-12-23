4f3a69da9ea6d127e91dd7ea6c363a0bd35c76aa
terra <phills2@hanmail.net>
2025-12-23 22:10:07 +0900

    1

--- kis_stock_order_complete.py
+++ kis_stock_order_complete.py
@@ -8,8 +8,8 @@ from decimal import Decimal
 URL_BASE = "https://openapi.koreainvestment.com:9443"       # 실전서비스
 
 # PostgreSQL 연결 설정
-# conn_string = "dbname='fund_risk_mng' host='localhost' port='5432' user='postgres' password='sktl2389!1'"
-conn_string = "dbname='fund_risk_mng' host='192.168.50.81' port='5432' user='postgres' password='asdf1234'"
+conn_string = "dbname='fund_risk_mng' host='localhost' port='5432' user='postgres' password='sktl2389!1'"
+# conn_string = "dbname='fund_risk_mng' host='192.168.50.81' port='5432' user='postgres' password='asdf1234'"
 # DB 연결
 conn = db.connect(conn_string)
 
