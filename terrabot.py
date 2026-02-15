17467d3e4b32a8a1653ec7eed993a2d356c58842
LeeSeungPhill <125461810+LeeSeungPhill@users.noreply.github.com>
2026-02-16 00:17:19 +0900

    종목코드 입력 수정

--- terrabot.py
+++ terrabot.py
@@ -5850,8 +5850,8 @@ def echo(update, context):
 
     # 입력메시지가 6자리 이상인 경우,
     if len(user_text) >= 6:
-        # 입력메시지가 앞의 6자리가 숫자인 경우,
-        if user_text[:6].isdecimal():
+        # 입력메시지가 앞의 1자리가 숫자인 경우,
+        if user_text[:1].isdecimal():
             # 입력메시지가 종목코드에 존재하는 경우
             if len(stock_code[stock_code.code == user_text[:6]].values) > 0:
                 code = stock_code[stock_code.code == user_text[:6]].code.values[0].strip()  ## strip() : 공백제거
