6c80a780d85b2fa5ca9dd740235285e848504d7b
LeeSeungPhill <125461810+LeeSeungPhill@users.noreply.github.com>
2026-04-11 07:48:23 +0900

    1

--- kis_stock_minute_save.py
+++ kis_stock_minute_save.py
@@ -133,60 +133,61 @@ def stock_minute_get(access_token, app_key, app_secret, code, company):
     
     cur1 = conn.cursor()
 
-    for i in range(circle):
+    for _ in range(circle):
         if hms > startHms:
             a = pd.DataFrame(inquire_time_itemchartprice(access_token, app_key, app_secret, code, hms))
 
-            for i, name in enumerate(a.index):
+            for idx, _ in enumerate(a.index):
 
-                trTime = (datetime.strptime(a['stck_cntg_hour'][i][:4], '%H%M') + timedelta(minutes=1)).strftime('%H%M')
+                trTime = (datetime.strptime(a['stck_cntg_hour'][idx][:4], '%H%M') + timedelta(minutes=1)).strftime('%H%M')
 
                 if low_price == 0:
-                    low_price = int(a['stck_lwpr'][i])
-                elif low_price > int(a['stck_lwpr'][i]):
-                    low_price = int(a['stck_lwpr'][i])
+                    low_price = int(a['stck_lwpr'][idx])
+                elif low_price > int(a['stck_lwpr'][idx]):
+                    low_price = int(a['stck_lwpr'][idx])
 
                 if high_price == 0:
-                    high_price = int(a['stck_hgpr'][i])
-                elif high_price < int(a['stck_hgpr'][i]):
-                    high_price = int(a['stck_hgpr'][i])
+                    high_price = int(a['stck_hgpr'][idx])
+                elif high_price < int(a['stck_hgpr'][idx]):
+                    high_price = int(a['stck_hgpr'][idx])
 
 
                 if trTime[2:] == '11' or trTime[2:] == '21' or trTime[2:] == '31' or trTime[2:] == '41' or trTime[2:] == '51' or trTime[2:] == '01':
-                    
-                    open_price = int(a['stck_oprc'][i])
-                    accum_vol += int(a['cntg_vol'][i])
+
+                    open_price = int(a['stck_oprc'][idx])
+                    accum_vol += int(a['cntg_vol'][idx])
 
                     tenMinuteUnit = (datetime.strptime(trTime, '%H%M') + timedelta(minutes=9)).strftime('%H%M')
 
-                    print(code + " : " + a['stck_bsop_date'][i] + str(tenMinuteUnit) + ", 현재가 : " + str(close_price) + ", 시가 : " + str(open_price) + ", 저가 : " + str(low_price) + ", 고가 : " + str(high_price) +", 체결 거래량 : " + str(accum_vol)) 
-                    
+                    print(code + " : " + a['stck_bsop_date'][idx] + str(tenMinuteUnit) + ", 현재가 : " + str(close_price) + ", 시가 : " + str(open_price) + ", 저가 : " + str(low_price) + ", 고가 : " + str(high_price) +", 체결 거래량 : " + str(accum_vol))
+
                     insert_query1 = "with upsert as (update stock_minute_info set current_price = %s, open_price = %s, high_price = %s, low_price = %s, volumn = %s, last_chg_date = %s where dt = %s and code = %s returning * ) insert into stock_minute_info(dt, name, code, current_price, open_price, high_price, low_price, volumn, last_chg_date) select %s, %s, %s, %s, %s, %s, %s, %s, %s where not exists(select * from upsert)"
                     # insert 인자값 설정
-                    record_to_insert1 = ([close_price, open_price, high_price, low_price, accum_vol, datetime.now(), a['stck_bsop_date'][i] + str(tenMinuteUnit), code,
-                                        a['stck_bsop_date'][i] + str(tenMinuteUnit), company, code, close_price, open_price, high_price, low_price, accum_vol, datetime.now()])
+                    record_to_insert1 = ([close_price, open_price, high_price, low_price, accum_vol, datetime.now(), a['stck_bsop_date'][idx] + str(tenMinuteUnit), code,
+                                        a['stck_bsop_date'][idx] + str(tenMinuteUnit), company, code, close_price, open_price, high_price, low_price, accum_vol, datetime.now()])
                     # DB 연결된 커서의 쿼리 수행
                     cur1.execute(insert_query1, record_to_insert1)
-                    conn.commit() 
+                    conn.commit()
 
                     low_price = 0
                     high_price = 0
                     accum_vol = 0
+                    close_price = 0
 
                 elif trTime[2:] == '10' or trTime[2:] == '20' or trTime[2:] == '30' or trTime[2:] == '40' or trTime[2:] == '50' or trTime[2:] == '00':
 
-                    close_price = int(a['stck_prpr'][i])                
-                    accum_vol += int(a['cntg_vol'][i])
-                    
+                    close_price = int(a['stck_prpr'][idx])
+                    accum_vol += int(a['cntg_vol'][idx])
+
                 else:
-                    accum_vol += int(a['cntg_vol'][i])
+                    accum_vol += int(a['cntg_vol'][idx])
 
             hms = (datetime.strptime((stock_day + hms), '%Y%m%d%H%M%S') - timedelta(minutes=30)).strftime('%H%M%S')
 
     cur1.close()
 
 cur0 = conn.cursor()
-cur0.execute("select name from stock_holiday where holiday = '"+today+"'")
+cur0.execute("SELECT name FROM stock_holiday WHERE holiday = %s", (today,))
 result_one = cur0.fetchone()
 cur0.close()
 
@@ -204,7 +205,11 @@ if result_one == None:
 
             # 잔고정보 및 관심종목 조회
             cur100 = conn.cursor()
-            cur100.execute("select code, name from \"stockBalance_stock_balance\" where acct_no = '" + str(acct_no) + "' and proc_yn = 'Y' UNION ALL select code, name from \"interestItem_interest_item\" where acct_no = '" + str(acct_no) + "' and length(code) > 4")
+            cur100.execute("""
+                SELECT code, name FROM "stockBalance_stock_balance" WHERE acct_no = %s AND proc_yn = 'Y'
+                UNION ALL
+                SELECT code, name FROM "interestItem_interest_item" WHERE acct_no = %s AND length(code) > 4
+            """, (str(acct_no), str(acct_no)))
             result_one00 = cur100.fetchall()
             cur100.close()
 
